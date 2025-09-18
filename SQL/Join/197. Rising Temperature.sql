3e9087a4e4991cdbfc5d6ab46ff1aa2603ed55a8
김현태 <kht1310@naver.com>
2025-09-18 01:29:06 -0400

    Rising Temperature

---
+++ SQL/Join/197. Rising Temperature.sql
@@ -0,0 +1,13 @@
+################################################################
+# Difficulty: Easy
+# Problem Link: https://leetcode.com/problems/rising-temperature/
+################################################################
+
+
+
+
+# Write your MySQL query statement below
+SELECT w2.id
+FROM Weather w1, Weather w2
+WHERE DATEDIFF(w2.recordDate, w1.recordDate) = 1
+AND w2.temperature > w1.temperature
