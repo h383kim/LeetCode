ad6304b23c5933d3e044fa1e814c45ef5bfa1a59
김현태 <kht1310@naver.com>
2025-09-19 00:13:59 -0400

    Confirmation Rate

---
+++ SQL/Join/1934. Confirmation Rate.sql
@@ -0,0 +1,25 @@
+################################################################
+# Difficulty: Medium
+# Problem Link: https://leetcode.com/problems/confirmation-rate/
+################################################################
+
+
+
+
+# Write your MySQL query statement below
+SELECT s.user_id,
+    ROUND(SUM(
+    CASE 
+        WHEN c.action = 'confirmed' THEN 1
+        ELSE 0
+    END
+    ) / 
+    CASE 
+        WHEN COUNT(c.action) = 0 THEN 1
+        ELSE COUNT(c.action)
+    END
+    , 2) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c
+ON s.user_id = c.user_id
+GROUP BY s.user_id
