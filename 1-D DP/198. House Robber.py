3180260a8df324f69f1927d8458abfb392428744
김현태 <kht1310@naver.com>
2025-10-07 03:29:22 -0400

    House Robber

---
+++ 1-D DP/198. House Robber.py
@@ -0,0 +1,20 @@
+################################################################
+# Difficulty: Medium
+# Problem Link: https://leetcode.com/problems/house-robber/
+################################################################
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        dp = [0] * len(nums)
+
+        for i in range(len(nums)):
+            if i == 0:
+                dp[i] = nums[i]
+            elif i == 1:
+                dp[i] = max(nums[0], nums[1])
+            else:
+                dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
+
+        return dp[len(nums) - 1]
